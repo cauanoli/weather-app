7a6a0c2ae7d6b2983f1af8452f0184a8be880e95
Cauan <cauandeoliveiracosta@gmail.com>
2024-06-25 14:46:17 -0300

    feat: style hour buttons

--- src/pages/weather/components/renderForecastDays.js
+++ src/pages/weather/components/renderForecastDays.js
@@ -4,6 +4,12 @@ export function renderForecastDays({ days, current, active }) {
   const container = document.createElement("div");
   container.classList = "days container";
 
+  const title = document.createElement("h2");
+  title.innerText = "Week climate";
+  title.classList = "days__title";
+
+  container.appendChild(title);
+
   days.forEach((day) => {
     const dayButton = createDayButton({
       day,
--- src/pages/weather/components/renderHours.js
+++ src/pages/weather/components/renderHours.js
@@ -4,10 +4,20 @@ export function renderHours({ data, activeHour }) {
   const container = document.createElement("div");
   container.classList = "hours-cards container";
 
+  const title = document.createElement("h2");
+  title.innerText = "Climate today";
+  title.classList = "hours-cards__title";
+
+  const cards = document.createElement("div");
+  cards.classList = "hours-cards__cards";
+
   data.hour.forEach((hour) => {
     const card = createHourCard({ hour, activeHour });
-    container.appendChild(card);
+    cards.appendChild(card);
   });
+
+  container.appendChild(title);
+  container.appendChild(cards);
 
   return container;
 }
